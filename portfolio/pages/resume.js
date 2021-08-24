d2400780427411563d43236cd79499af0544a280
Avinash Rao <avinashrao2002@gmail.com>
2021-08-23 20:29:19 -0700

    Footer for Resume

--- portfolio/pages/resume.js
+++ portfolio/pages/resume.js
@@ -28,6 +28,23 @@ export default function resume() {
             />
           </main>
         </div>
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Built by Avinash Rao with Next.js and
+            <span className={styles.logo}>
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </span>
+          </a>
+        </footer>
       </div>
     </>
   );
